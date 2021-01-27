b9a38bd931b6b91271a313aab3161b0550e563ca
THOMAS MAIRE <thomas.maire@cheops.fr>
2021-01-27 11:30:23 +0100

    Ajout d'un event form sur le formulaire commentaire

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -6,10 +6,21 @@ use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
 
 class CommentType extends AbstractType
 {
+
+    private $user;
+
+    public function __construct(Security $security)
+    {
+        $this->user = $security->getUser();
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -24,6 +35,14 @@ class CommentType extends AbstractType
                 'label' => "Accepter les conditions"
             ])
         ;
+
+        $builder->addEventListener(FormEvents::POST_SET_DATA, function(FormEvent $event){
+            $form = $event->getForm();
+            if($this->user !== null){
+                $form->get('author')->setData($this->user->getUsername());
+            }
+        });
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
